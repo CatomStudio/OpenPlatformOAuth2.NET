37c8005f6eea9dd5de5ce48ccbc5adb565f91d29
sanxia <gerenxing@qq.com>
2014-11-28 11:16:25 +0800

    add files

---
+++ GeRenXing.OpenPlatform/IOAuthClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeRenXing.OpenPlatform
+{
+    /// <summary>
+    /// Description: IOAuth接口
+    /// Author: 美丽的地球
+    /// Email: sanxia330@qq.com
+    /// QQ: 1851690435
+    /// </summary>
+    public interface IOAuthClient
+    {
+        AuthOption Option { get; }
+        AuthToken Token { get; }
+        IUserInterface User { get; }
+
+        String GetAuthorizeUrl(ResponseType responseType);
+        AuthToken GetAccessTokenByAuthorizationCode(string code);
+        AuthToken GetAccessTokenByPassword(string passport, string password);
+        AuthToken GetAccessTokenByRefreshToken(string refreshToken);
+        String Get(String url, params RequestOption[] options);
+        String Post(String url, params RequestOption[] options);
+    }
+
+}
---
+++ GeRenXing.OpenPlatform/IUserInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeRenXing.OpenPlatform
+{
+    /// <summary>
+    /// OAuth基础用户接口
+    /// Author: 美丽的地球
+    /// Email: sanxia330@qq.com
+    /// QQ: 1851690435
+    /// </summary>
+    public interface IUserInterface
+    {
+        dynamic GetUserInfo();
+        void EndSession();
+    }
+
+}
